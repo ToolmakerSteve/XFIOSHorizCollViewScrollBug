c8285ec05db456a1c50febe8334448b0b172a614
ToolmakerSteve <ToolmakerSteve@users.noreply.github.com>
2021-12-16 14:16:18 -0600

    initial commit

---
+++ XFIOSHorizCollViewScrollBug/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFIOSHorizCollViewScrollBug
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+			InitRoomList();
+			BindingContext = this;
+		}
+
+		public ObservableCollection<Room> RoomList { get; set; }
+
+		private Room _previousSelection;
+
+		private void RoomCollectionSelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			var selectedItem = e.CurrentSelection.FirstOrDefault() as Room;
+			SelectRoom(selectedItem);
+		}
+
+		private void SelectRoom(Room room)
+		{
+			if (room != null) {
+				if (_previousSelection != null)
+					_previousSelection.IsSelected = false;
+
+				room.IsSelected = true;
+				rooms_List.ScrollTo(room, position: ScrollToPosition.Center, animate: false);
+
+				_previousSelection = room;
+			}
+		}
+
+		string[] roomNames = new string[] {
+			"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight"
+		};
+
+		private void InitRoomList()
+		{
+			var rooms = new ObservableCollection<Room>();
+			foreach (var name in roomNames) {
+				rooms.Add(new Room(name));
+			}
+
+			var room = rooms[0];
+			room.IsSelected = true;
+			_previousSelection = room;
+
+			RoomList = rooms;
+		}
+	}
+}
---
+++ XFIOSHorizCollViewScrollBug/Room.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XFIOSHorizCollViewScrollBug
+{
+	public class Room : Xamarin.Forms.BindableObject
+	{
+
+		public string RoomName { get; set; }
+
+		public bool IsSelected {
+			get => _isSelected;
+			set {
+				_isSelected = value;
+				OnPropertyChanged();
+			}
+		}
+		private bool _isSelected;
+
+		public Room(string name, bool isSelected = false)
+		{
+			RoomName = name;
+			IsSelected = isSelected;
+		}
+	}
+}
